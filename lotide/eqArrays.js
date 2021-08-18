2fb23713534522769472df82daea882411fd337e
Anshu Pasrija <anshu.pasrija@gmail.com>
2021-08-17 19:17:59 -0700

    comparing array

---
+++ lotide/eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+ 
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:, ${actual} === ${expected}`);
+  }  else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays=function(num1,num2){
+  if(num1.length !== num2.length)
+  return false;
+  for(let i=0;i<num1.length;i++) {
+    if(num1[i] !==num2[i]) {
+      return false;
+    }
+  } return true;
+
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
