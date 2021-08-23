98808e0435b98f46b0c6ae50a2958683b9b579eb
anshu pasrija <anshu.pasrija@gmail.com>
2021-08-23 23:53:01 +0000

    refactoring assertArraysEqual

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,15 +1,6 @@
-const eqArrays=function(num1,num2){
-  if(num1.length !== num2.length){
-  return false;
-  }
-  for(let i=0;i<num1.length;i++) {
-    if(num1[i] !==num2[i]) {
-      return false;
-    }
-  } 
-  return true;
 
-};
+
+const eqArrays = require('./eqArrays');
 const assertArraysEqual=function(arg1,arg2) {
   if (eqArrays(arg1,arg2)) {
     console.log(`✅✅✅ Assertion Passed:, ${arg1} === ${arg2}`);
@@ -17,5 +8,6 @@ const assertArraysEqual=function(arg1,arg2) {
     console.log(`🛑🛑🛑 Assertion Failed: ${arg1} !== ${arg2}`);
   }
 };
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[3,2,3]);
+
+
+module.exports=assertArraysEqual;
