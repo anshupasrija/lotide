494101f733050060ab91f72ab1ed4b8077f4db0b
anshu pasrija <anshu.pasrija@gmail.com>
2021-08-24 00:59:05 +0000

    Incorporate Mocha and Chai

--- lotide/head.js
+++ lotide/head.js
@@ -1,16 +1,3 @@
-// const assertEqual = function(actual, expected) {
- 
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed:, ${actual} === ${expected}`);
-//   }  else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-// TEST CODE
-//assertEqual("Lighthouse Labs", "Lighthouse Labs");
-// assertEqual(10, 1);
-
 const assertEqual =require('./assertEqual');
 const head=function(num)
 {
